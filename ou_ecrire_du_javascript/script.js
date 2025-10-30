6ba328d4c41265c758c501c6857954a5d8d0024c
Modou Niane <mniane2005@gmail.com>
2025-10-30 21:26:58 +0000

    first commit

---
+++ ou_ecrire_du_javascript/script.js
@@ -0,0 +1,7 @@
+function changerTest() {
+            document.getElementsByTagName("p")[0].innerHTML = "Allah is the most great";
+            //[0] pour cibler le premier paragraphe
+        }
+function change(){
+    document.getElementsByTagName("h1")[0].innerHTML = "call to action";
+}
