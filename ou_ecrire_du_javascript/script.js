a3f0e9c9492990b94844c75aba6f5b898c3e7297
Modou Niane <mniane2005@gmail.com>
2025-12-02 13:54:45 +0000

    Ajout de nouveaux fichiers puissants

--- ou_ecrire_du_javascript/script.js
+++ ou_ecrire_du_javascript/script.js
@@ -1,9 +1,10 @@
 function changerTest() {
             document.getElementsByTagName("p")[0].innerHTML = "Allah is the most great";
             //[0] pour cibler le premier paragraphe
+
         }
 function change(){
-    document.getElementsByTagName("h1")[0].innerHTML = "call to action";
+    document.getElementsByTagName("h1")[0].innerHTML = "call the action";
     let a = 3;// variable locale
     var b = 10;//variable globale
     const pi = 3.14;
